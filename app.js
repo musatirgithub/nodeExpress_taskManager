66faf86bd31f23c2d395eb4e393c5aaebc7f2105
Mehmet Ugur Satir <musatir@gmail.com>
2023-08-14 22:36:15 +0200

    async wrapper

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not-found');
 
 app.use(express.static('./public'));
 app.use(express.json());
 app.use('/api/v1/tasks',tasks);
+app.use(notFound);
 
 const port = 3000;
 
